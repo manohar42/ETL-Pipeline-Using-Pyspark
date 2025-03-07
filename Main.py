52fe17850d857278d156ae95c679e8af6418a4e3
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-07 01:37:17 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,41 @@
+from pyspark.conf import SparkConf
+from pyspark.sql import SparkSession
+from LoadData import LoadData
+from dataCleaning import dataCleaning
+
+
+class Spark:
+
+    def __init__(self):
+        self.conf = SparkConf()
+        self.conf.set("app.name","ETLPipeline") \
+                .set("master","local[2]")
+        self.spark = SparkSession.builder \
+                .config(conf=self.conf) \
+                .getOrCreate()
+    def loadData(self):
+        self.data = LoadData(self.spark).return_data()
+        self.data.show()
+        print("\n✅ Loaded raw data:")
+
+    def data_cleaning(self):
+        DataClean = dataCleaning(self.spark)
+        missing_values = DataClean.check_missing_values(self.data)
+        if missing_values.count() > 0:
+            print("\n⚠️ Found missing values in product_id or store_id:")
+            missing_values.show()
+        else:
+            print("\n✅ No missing values in product_id or store_id.")
+
+
+
+
+
+
+if __name__ == "__main__":
+    s = Spark()
+    # Step 1: Load the Raw Data
+    s.loadData()
+    # Step 2: Data Cleaning
+    ## 2.1: Checking for Missing Values in Key Attributes (product_id & store_id)
+    s.data_cleaning()
